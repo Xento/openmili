a52d6ce394cca490f8e1e86955e77d0d22517fe7
Xento <markus-mi@web.de>
2017-02-11 20:22:30 +0100

    Fixed merging error

--- openmili.ino
+++ openmili.ino
@@ -47,78 +47,78 @@ void loop()
       for (int i = 0; i < packet_length; i++) {
         printf("%02X ", packet[i]);
       }
-    }
 
-    int dupesReceived = mlr.dupesReceived();
-    for (; dupesPrinted < dupesReceived; dupesPrinted++) {
-      printf(".");
-    }
-    
-    printf("\n");
-    
-    printf("ID %02X%02X;", packet[1], packet[2]);
-    
-    if(packet[4] & 0b00000111) 
-      printf("Group %d;", packet[4] & 0b00000111);
-    else
-      printf("All;");
-    
-    byte button = packet[5] & 0x0F;
-    
-    if(button == 0x0F) 
-      printf("Color %02X;", packet[3]);
-    else if(button == 0x0E) {
-      byte brightnes = (packet[4] & 0b11111000) >> 3;
+      int dupesReceived = mlr.dupesReceived();
+      for (; dupesPrinted < dupesReceived; dupesPrinted++) {
+        printf(".");
+      }
       
-      if(brightnes <= 18) {
+      printf("\n");
+      
+      printf("ID %02X%02X;", packet[1], packet[2]);
+      
+      if(packet[4] & 0b00000111) 
+        printf("Group %d;", packet[4] & 0b00000111);
+      else
+        printf("All;");
+      
+      byte button = packet[5] & 0x0F;
+      
+      if(button == 0x0F) 
+        printf("Color %02X;", packet[3]);
+      else if(button == 0x0E) {
+        byte brightnes = (packet[4] & 0b11111000) >> 3;
+        
+        if(brightnes <= 18) {
         brightnes -= 16;
         brightnes = brightnes * -1;
-      }
-      else{
+        }
+        else{
         brightnes -= 47;
         brightnes = brightnes * -1;
-      }
-      
-      if(brightnes < 0)
+        }
+        
+        if(brightnes < 0)
         brightnes = 0;
-      else if(brightnes > 25)
+        else if(brightnes > 25)
         brightnes = 25;
-      
-      printf("Brightnes %d;", brightnes);
+        
+        printf("Brightnes %d;", brightnes);
+      }
+      else if(button == 0x0D) 
+        printf("Disco %d;", packet[0] & 0x0F);
+      else if(button == 0x0C) 
+        printf("Speed -;");
+      else if(button == 0x0B) 
+        printf("Speed +;");
+      else if(button == 0x0A) 
+        printf("Group 4 off;");
+      else if(button == 0x09) 
+        printf("Group 4 on;");
+      else if(button == 0x08) 
+        printf("Group 3 off;");
+      else if(button == 0x07) 
+        printf("Group 3 on;");
+      else if(button == 0x06) 
+        printf("Group 2 off;");
+      else if(button == 0x05) 
+        printf("Group 2 on;");
+      else if(button == 0x04)
+        printf("Group 1 off;");
+      else if(button == 0x03)
+        printf("Group 1 on;");
+      else if(button == 0x02)
+        printf("All off;");
+      else if(button == 0x01)
+        printf("All on;");
+      else if(button == 0x00) 
+        printf("Slider released;");
+        
+      if(packet[5] & 0x10)
+        printf("Long press;");
+      else
+        printf("Short press;");
     }
-    else if(button == 0x0D) 
-      printf("Disco %d;", packet[0] & 0x0F);
-    else if(button == 0x0C) 
-      printf("Speed -;");
-    else if(button == 0x0B) 
-      printf("Speed +;");
-    else if(button == 0x0A) 
-      printf("Group 4 off;");
-    else if(button == 0x09) 
-      printf("Group 4 on;");
-    else if(button == 0x08) 
-      printf("Group 3 off;");
-    else if(button == 0x07) 
-      printf("Group 3 on;");
-    else if(button == 0x06) 
-      printf("Group 2 off;");
-    else if(button == 0x05) 
-      printf("Group 2 on;");
-    else if(button == 0x04)
-      printf("Group 1 off;");
-    else if(button == 0x03)
-      printf("Group 1 on;");
-    else if(button == 0x02)
-      printf("All off;");
-    else if(button == 0x01)
-      printf("All on;");
-    else if(button == 0x00) 
-      printf("Slider released;");
-      
-    if(packet[5] & 0x10)
-      printf("Long press;");
-    else
-      printf("Short press;");
   }
 
   while (Serial.available()) {
